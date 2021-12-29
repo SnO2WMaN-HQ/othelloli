3d5278d22b1f4027181d9d565ea5021e679ca6d7
SnO2WMaN <me@sno2wman.net>
2021-12-29 17:09:11 +0900

    othello rule goes by

--- websocket-server/src/board.ts
+++ websocket-server/src/board.ts
@@ -7,76 +7,78 @@ export type Board = {
   stones: number[];
 };
 
+export const STONE_NONE = -1;
+
 export const createInitStones = (
   width: number,
   height: number,
   colors: number,
 ): Board["stones"] => {
   return [
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
     0,
     1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
     1,
     0,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
+    STONE_NONE,
   ];
 };
 
@@ -90,16 +92,86 @@ export const getColors = (board: Board): number[] => {
 
 export const createNewBoard = (
   board: Board,
-  index: number,
+  placeable: number[],
 ): Board => {
   const stones = board.stones;
-  stones[index] = board.role;
+  placeable.forEach((index) => {
+    stones[index] = board.role;
+  });
 
   return {
     ...board,
     stones: stones,
     role: (board.role + 1) % getColors(board).length,
   };
+};
+
+export const calcBoardIndex = (board: Board, x: number, y: number) =>
+  y * board.height + x;
+
+export type Directions =
+  | [-1, -1]
+  | [-1, 0]
+  | [-1, 1]
+  | [0, -1]
+  | [0, 1]
+  | [1, -1]
+  | [1, 0]
+  | [1, 1];
+
+export const recur = (
+  board: Board,
+  x: number,
+  y: number,
+  role: number,
+  dir: Directions,
+  index: number,
+): number => {
+  const dx = x + dir[0] * index;
+  const dy = y + dir[1] * index;
+
+  if (dx < 0 || board.width <= dx || dy < 0 || board.height <= dy) return 0;
+  else if (board.stones[calcBoardIndex(board, dx, dy)] === STONE_NONE) return 0;
+  else if (board.stones[calcBoardIndex(board, dx, dy)] === role) {
+    return index === 1 ? 0 : index;
+  } else {
+    return recur(board, x, y, role, dir, index + 1);
+  }
+};
+
+export const calcPlaceableByDirection = (
+  board: Board,
+  x: number,
+  y: number,
+  role: number,
+  dir: Directions,
+): number[] =>
+  [...new Array(recur(board, x, y, role, dir, 1))]
+    .map((_, i) =>
+      calcBoardIndex(board, x + (i + 1) * dir[0], y + (i + 1) * dir[1])
+    );
+
+export const calcPlaceable = (
+  board: Board,
+  role: number,
+  x: number,
+  y: number,
+): number[] => {
+  if (board.stones[calcBoardIndex(board, x, y)] !== STONE_NONE) return [];
+
+  const round = [
+    ...calcPlaceableByDirection(board, x, y, role, [-1, -1]),
+    ...calcPlaceableByDirection(board, x, y, role, [-1, 0]),
+    ...calcPlaceableByDirection(board, x, y, role, [-1, 1]),
+    ...calcPlaceableByDirection(board, x, y, role, [0, -1]),
+    ...calcPlaceableByDirection(board, x, y, role, [0, 1]),
+    ...calcPlaceableByDirection(board, x, y, role, [1, -1]),
+    ...calcPlaceableByDirection(board, x, y, role, [1, 0]),
+    ...calcPlaceableByDirection(board, x, y, role, [1, 1]),
+  ];
+
+  if (1 <= round.length) return [calcBoardIndex(board, x, y), ...round];
+  return [];
 };
 
 export const updateBoard = (
@@ -113,7 +185,7 @@ export const updateBoard = (
     & { status: "bad" }
     & { message: "NOT_PLAYER" | "NOT_YOUR_TURN" | "CANNOT_PLACE_HERE" }
   ) => {
-  if (!Object.keys(board.players).includes(playerId)) {
+  if (!getPlayersIds(board).includes(playerId)) {
     return { status: "bad", message: "NOT_PLAYER" };
   }
 
@@ -122,8 +194,11 @@ export const updateBoard = (
     return { status: "bad", message: "NOT_YOUR_TURN" };
   }
 
-  const index = board.height * y + x;
-  return { "status": "ok", data: createNewBoard(board, index) };
+  const placeable = calcPlaceable(board, board.role, x, y);
+  if (placeable.length < 1) {
+    return { status: "bad", message: "CANNOT_PLACE_HERE" };
+  }
+  return { "status": "ok", data: createNewBoard(board, placeable) };
 };
 
 export const countStones = (
